eb75161ef891ab4b5b2c5c3abcd1f363c84802f2
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2024-01-07 10:40:16 -0800

    Move api into data access layer

--- migrations/1704300326910_initial-migration.js
+++ migrations/1704300326910_initial-migration.js
@@ -2,8 +2,8 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {
-  pgm.sql(`
+exports.up = (pgm) => {
+	pgm.sql(`
     CREATE EXTENSION "uuid-ossp";
 
     CREATE SCHEMA "auth";
@@ -32,7 +32,6 @@ exports.up = pgm => {
       "rental_code" TEXT,
       "description" TEXT,
       "body" TEXT,
-      "upvotes" INTEGER,
       "created_at" TIMESTAMPTZ DEFAULT (now()),
       "updated_at" TIMESTAMPTZ DEFAULT (now()),
       "user_id" UUID NOT NULL,
@@ -47,18 +46,18 @@ exports.up = pgm => {
       "item" TEXT,
       "moves" TEXT[],
       "nature" TEXT NOT NULL,
-      "hp_evs" SMALLINT,
-      "atk_evs" SMALLINT,
-      "def_evs" SMALLINT,
-      "spa_evs" SMALLINT,
-      "spd_evs" SMALLINT,
-      "speed_evs" SMALLINT,
-      "hp_ivs" SMALLINT,
-      "atk_ivs" SMALLINT,
-      "def_ivs" SMALLINT,
-      "spa_ivs" SMALLINT,
-      "spd_ivs" SMALLINT,
-      "speed_ivs" SMALLINT,
+      "hp_evs" SMALLINT NOT NULL DEFAULT 0,
+      "atk_evs" SMALLINT NOT NULL DEFAULT 0,
+      "def_evs" SMALLINT NOT NULL DEFAULT 0,
+      "spa_evs" SMALLINT NOT NULL DEFAULT 0,
+      "spd_evs" SMALLINT NOT NULL DEFAULT 0,
+      "speed_evs" SMALLINT NOT NULL DEFAULT 0,
+      "hp_ivs" SMALLINT NOT NULL DEFAULT 0,
+      "atk_ivs" SMALLINT NOT NULL DEFAULT 0,
+      "def_ivs" SMALLINT NOT NULL DEFAULT 0,
+      "spa_ivs" SMALLINT NOT NULL DEFAULT 0,
+      "spd_ivs" SMALLINT NOT NULL DEFAULT 0,
+      "speed_ivs" SMALLINT NOT NULL DEFAULT 0,
       "extra_data" JSONB,
       "thread_id" UUID NOT NULL,
       constraint "post_pokemon_pkey" PRIMARY KEY (id),
@@ -68,7 +67,6 @@ exports.up = pgm => {
     CREATE TABLE "post"."comment" (
       "id" UUID NOT NULL default uuid_generate_v4(),
       "body" TEXT NOT NULL,
-      "upvotes" INTEGER,
       "created_at" TIMESTAMPTZ DEFAULT (CURRENT_TIMESTAMP),
       "updated_at" TIMESTAMPTZ DEFAULT (now()),
       "user_id" UUID NOT NULL,
@@ -79,23 +77,43 @@ exports.up = pgm => {
       constraint "post_pokemon_parent_comment_id_fkey" foreign key ("parent_comment_id") references "post"."comment" on update cascade on delete cascade,
       constraint "post_comment_thread_id_fkey" foreign key ("thread_id") references "post"."thread" on update cascade on delete cascade
     );
-  `)
+
+    CREATE TABLE "post"."thread_upvote" (
+      "id" UUID NOT NULL default uuid_generate_v4(),
+      "user_id" UUID NOT NULL,
+      "thread_id" UUID NOT NULL,
+      constraint "post_thread_upvote_pkey" PRIMARY KEY (id),
+      constraint "post_thread_upvote_user_id_fkey" foreign key ("user_id") references "auth"."user" on update cascade on delete cascade,
+      constraint "post_thread_upvote_thread_id_fkey" foreign key ("thread_id") references "post"."thread" on update cascade on delete cascade
+    );
+
+    CREATE TABLE "post"."comment_upvote" (
+      "id" UUID NOT NULL default uuid_generate_v4(),
+      "user_id" UUID NOT NULL,
+      "comment_id" UUID NOT NULL,
+      constraint "post_comment_upvote_pkey" PRIMARY KEY (id),
+      constraint "post_comment_upvote_user_id_fkey" foreign key ("user_id") references "auth"."user" on update cascade on delete cascade,
+      constraint "post_comment_upvote_comment_id_fkey" foreign key ("comment_id") references "post"."comment" on update cascade on delete cascade
+    );
+  `);
 };
 
-exports.down = pgm => {
-  pgm.sql(`
+exports.down = (pgm) => {
+	pgm.sql(`
     -- Drop tables
+    DROP TABLE IF EXISTS "post"."thread_upvote";
+    DROP TABLE IF EXISTS "post"."comment_upvote";
     DROP TABLE IF EXISTS "post"."comment";
     DROP TABLE IF EXISTS "post"."pokemon";
     DROP TABLE IF EXISTS "post"."thread";
     DROP TABLE IF EXISTS "auth"."session";
     DROP TABLE IF EXISTS "auth"."user";
-    
+
     -- Drop schemas
     DROP SCHEMA IF EXISTS "post" CASCADE;
     DROP SCHEMA IF EXISTS "auth" CASCADE;
-    
+
     -- Drop extension
     DROP EXTENSION IF EXISTS "uuid-ossp";
-  `)
+  `);
 };
