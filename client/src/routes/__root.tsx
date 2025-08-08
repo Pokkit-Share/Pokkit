44c22562e0f243b6ac03bb95ba02c03adb61442b
LoreAsh <220929080+LoreAsh@users.noreply.github.com>
2025-08-08 10:21:37 -0700

    Fix: change casing, matters on Linux but not Windows

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import Navbar from '@components/Navbar';
+import Navbar from '@components/navbar';
 
 export const Route = createRootRoute({
 	component: () => (
